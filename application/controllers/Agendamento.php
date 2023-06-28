611548e95161a1ef86723427bce0a60ac51b09d1
Larissa Stefany <126928242+larissa-stefany@users.noreply.github.com>
2023-06-27 22:19:11 -0300

    .

--- application/controllers/Agendamento.php
+++ application/controllers/Agendamento.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Historico_cliente extends CI_Controller {
+class Agendamento extends CI_Controller {
 	public function __construct()
 	{
 	  parent::__construct();
