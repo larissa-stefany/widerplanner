61829a2038290b8743345adddbdeb03c641e9f14
Larissa Stefany <126928242+larissa-stefany@users.noreply.github.com>
2023-08-03 01:04:58 -0300

    jimin

--- application/views/agendamento.php
+++ application/views/agendamento.php
@@ -1,72 +1,135 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela de Agendamento</title>
     <style>
         body {
             font-family: Arial, sans-serif;
-        }
-        .container {
+            text-align: center;
             display: flex;
-            justify-content: center;
+            flex-direction: column;
             align-items: center;
+            justify-content: center;
             height: 100vh;
+            margin: 0;
         }
-        .form {
-            text-align: left;
-            width: 300px;
+        h2 {
+            font-size: 32px;
+            margin: 0;
         }
-        .form label {
-            display: block;
-            margin-bottom: 5px;
-        }
-        .form input[type="text"],
-        .form select {
+        form {
+            max-width: 400px;
             width: 100%;
-            padding: 10px;
-            margin-bottom: 10px;
+            padding: 20px;
+            box-sizing: border-box;
+            margin-top: 20px;
         }
-        .form input[type="submit"] {
-            padding: 10px;
+        label {
+            display: block;
+            margin-top: 10px; 
+            text-align: left;
+        }
+        .form-row {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 15px;
+        }
+        .form-row label {
+            flex-basis: 30%;
+            margin: 0;
+        }
+        .form-row input[type="text"],
+        .form-row input[type="tel"],
+        .form-row input[type="email"],
+        .form-row select,
+        .form-row textarea {
+            flex: 1;
+            padding: 8px;
+            margin-top: 5px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+        .form-row textarea {
+            resize: vertical;
+        }
+        .date-time-container {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 10px; 
+        }
+        .date-container,
+        .time-container {
+            flex: 1;
+            display: flex;
+            flex-direction: column; 
+            align-items: center;
+        }
+        .date-container label,
+        .time-container label {
+            display: block;
+        }
+        input[type="submit"] {
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            margin-top: 20px;
+        }
+        input[type="submit"]:hover {
+            background-color: #45a049;
         }
     </style>
 </head>
 <body>
-    <div class="container">
-        <div class="form">
-            <h2>Agendamento</h2>
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-                <label for="nome">Nome:</label>
-                <input type="text" id="nome" name="nome" required>
-                
-                <label for="email">Email:</label>
-                <input type="text" id="email" name="email" required>
-                
-                <label for="telefone">Telefone:</label>
-                <input type="text" id="telefone" name="telefone" required>
-
-                <div class="col-sm-6 col-sm-offset-3">
-                <label class="form-control">
-                <option>Serviços:</option>
-                <select name="servicos" class="form-control">
-                <option value="" selected=>Selecione um serviço</option>
-                <option>teste01 -- R$00,00</option>
-                <option>teste02 -- R$00,00</option> 
-                <option>teste03 -- R$00,00</option> 
-                <option>teste04 -- R$00,00</option> 
-                <option>teste05 -- Sob consulta</option>
-            </select> 
-                
+    <h2>Agendamento</h2>
+    <form action="processar_agendamento.php" method="post">
+        <div class="form-row">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" required>
+        </div>
+        <div class="form-row">
+            <label for="telefone">Telefone:</label>
+            <input type="tel" id="telefone" name="telefone" required>
+        </div>
+        
+        <div class="form-row">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+        </div>
+        
+        <div class="form-row">
+            <label for="servico">Serviço:</label>
+            <select id="servico" name="servico" required>
+                <option value="" disabled selected>Selecione um serviço</option>
+                <option value="servico1">Serviço 1</option>
+                <option value="servico2">Serviço 2</option>
+                <option value="servico3">Serviço 3</option>
+                <option value="servico4">Serviço 4</option>
+                <option value="servico5">Serviço 5</option>
+            </select>
+        </div>
+        
+        <div class="form-row date-time-container">
+            <div class="date-container">
                 <label for="data">Data:</label>
                 <input type="date" id="data" name="data" required>
-                
-                <label for="horario" style="margin-top: 10px;">Horário:</label>
-                <input type="time" id="horario" name="horario" required>
-
-                <input type="submit" value="Agendar">
-                
-            </form>
+            </div>
+            
+            <div class="time-container">
+                <label for="hora">Hora:</label>
+                <input type="time" id="hora" name="hora" required>
+            </div>
         </div>
-    </div>
+        
+        <input type="submit" value="Agendar">
+        <input type="reset" value="Limpar">
+    </form>
 </body>
 </html>
